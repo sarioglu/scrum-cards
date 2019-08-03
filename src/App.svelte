a7bc728c055b9cb8b8fcd895f5981a6450f49c68
Mehmet Sarıoğlu <mehmet.sarioglu@dcipheranalytics.com>
2019-08-03 19:57:11 +0300

    Add max-width to grid

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <style>
+  .wrapper {
+    width: 100%;
+    height: 100%;
 
+    background-color: #edf2f7;
+  }
 </style>
 
-<CardGrid />
+<div class="wrapper">
+  <CardGrid />
+</div>
--- src/components/CardGrid.svelte
+++ src/components/CardGrid.svelte
@@ -11,11 +11,35 @@
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: auto;
 
-    background-color: #edf2f7;
-
+    width: 100%;
     height: 100%;
+    margin: auto;
     padding: 1rem;
     box-sizing: border-box;
+  }
+
+  @media (min-width: 640px) {
+    .grid {
+      max-width: 640px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .grid {
+      max-width: 768px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .grid {
+      max-width: 1024px;
+    }
+  }
+
+  @media (min-width: 1280px) {
+    .grid {
+      max-width: 1280px;
+    }
   }
 </style>
 
