3823f0fbabfa81c3b9df5423fc6a5c876e8cdaf0
Mehmet Sarıoğlu <mehmet.sarioglu@dcipheranalytics.com>
2019-08-03 20:17:27 +0300

    Add Rubik font

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -8,8 +8,9 @@
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
     border-radius: 0.5rem;
 
+    font-family: 'Rubik', sans-serif;
     font-size: 6rem;
-    font-weight: bold;
+    font-weight: 500;
 
     transition: box-shadow 200ms ease-in;
   }
