65aa3d900e211111d445fec822261445aae1d385
Mehmet Sarıoğlu <mehmet.sarioglu@dcipheranalytics.com>
2019-10-23 11:35:14 +0300

    Update server base path

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === "development";
 
 polka() // You can also use Express
   .use(
+    '/sarioglumeh/scrum-cards',
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()
